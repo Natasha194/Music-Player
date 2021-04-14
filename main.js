213b91f0668229280d627234c0974446f796dcdd
Natasha194 <77156649+Natasha194@users.noreply.github.com>
2021-04-14 10:51:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function MrPerfectlyFine() {
+    document.getElementById("audio").src = "Taylor Swift - Mr. Perfectly Fine (Taylor’s Version) (From The Vault) (1).mp3";
+    document.getElementById("audio").play();
+}
+
+function Arcade() {
+    document.getElementById("audio").src = "Duncan Laurence - Arcade ft. FLETCHER (2).mp3";
+    document.getElementById("audio").play();
+}
+
+function agathaAllAlong() {
+    document.getElementById("audio").src = "Agatha All Along (From WandaVision Episode 7 Audio Only) (1).mp3";
+    document.getElementById("audio").play();
+}
+
+function Lonely() {
+    document.getElementById("audio").src = "Lonely - Justin Bieber FT. benny blanco (1).mp3";
+    document.getElementById("audio").play();
+}
+
+function Empire() {
+    document.getElementById("audio").src = "Ella Henderson - Empire (1).mp3";
+    document.getElementById("audio").play();
+}
+
+function Positions() {
+    document.getElementById("audio").src = "Ariana Grande - positions (2).mp3";
+    document.getElementById("audio").play();
+}
+
+function Cups() {
+    document.getElementById("audio").src = "CUPS - Pitch Perfect - Sam Tsui, Alex G, Kina Grannis, Kurt Schneider (1).mp3";
+    document.getElementById("audio").play();
+}
+
+function Firecracker() {
+    document.getElementById("audio").src = "Lost & Found Music Studios _ Firecracker (2).mp3";
+    document.getElementById("audio").play();
+}
